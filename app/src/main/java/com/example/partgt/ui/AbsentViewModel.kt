32bd4bd4be9f736ed50307b2702f0b364b6ed045
CharlesJKK <selrahcjunior@hotmail.com>
2023-05-02 13:17:39 -0300

    refactor: mudando nome das fragments e alterando tudo relacionado aos nomes antigos e criando uma nova activity.

--- app/src/main/java/com/example/partgt/ui/AbsentViewModel.kt
+++ app/src/main/java/com/example/partgt/ui/AbsentViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.partgt.ui.home
+package com.example.partgt.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class AbsentViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is slideshow Fragment"
     }
     val text: LiveData<String> = _text
 }
