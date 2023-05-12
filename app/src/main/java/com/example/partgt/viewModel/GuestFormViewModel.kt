c4631688a409312c06a4a3debeefeaecb9aa6a5f
CharlesJKK <selrahcjunior@hotmail.com>
2023-05-12 13:40:08 -0300

    feat: configuração inicial do banco de dados sqlite do android

--- app/src/main/java/com/example/partgt/viewModel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/partgt/viewModel/GuestFormViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.partgt.viewModel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.example.partgt.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel() {
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = GuestRepository.getInstance()
+    private val repository = GuestRepository.getInstance(application)
 
 
 
